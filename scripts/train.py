477721cf18d864a93675dc32dee632767d2dbb1f
Franco Ruggeri <fruggeri@kth.se>
2020-08-27 13:20:16 +0200

    add learning curves saving and model loading

--- scripts/train.py
+++ scripts/train.py
@@ -11,6 +11,7 @@ from tensorflow.keras.applications.resnet_v2 import preprocess_input
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from tensorflow.keras.models import load_model
 
 IMAGE_SHAPE = (224, 224)
 INPUT_SHAPE = IMAGE_SHAPE + (3,)
@@ -23,14 +24,13 @@ def get_dataset(dataset_path):
     return dataset
 
 
-def plot_learning_curves(history, history_ft=None):
+def plot_learning_curves(model_path, history, history_ft=None):
     if history_ft is not None:
         history.history['accuracy'] += history_ft.history['accuracy']
         history.history['loss'] += history_ft.history['loss']
         history.history['val_accuracy'] += history_ft.history['val_accuracy']
         history.history['val_loss'] += history_ft.history['val_loss']
 
-    plt.subplot(2, 1, 1)
     plt.plot(history.history['accuracy'], label='training')
     plt.plot(history.history['val_accuracy'], label='validation')
     plt.xlabel('epoch')
@@ -38,8 +38,9 @@ def plot_learning_curves(history, history_ft=None):
     if history_ft is not None:
         plt.plot([history.epoch[-1], history.epoch[-1]], plt.ylim(), label='start fine-tuning')
     plt.legend()
+    plt.savefig(model_path.with_name(model_path.stem + '_acc').with_suffix('.png'))
 
-    plt.subplot(2, 1, 2)
+    plt.figure()
     plt.plot(history.history['loss'], label='training')
     plt.plot(history.history['val_loss'], label='validation')
     plt.xlabel('epoch')
@@ -47,24 +48,11 @@ def plot_learning_curves(history, history_ft=None):
     if history_ft is not None:
         plt.plot([history.epoch[-1], history.epoch[-1]], plt.ylim(), label='start fine-tuning')
     plt.legend()
+    plt.savefig(model_path.with_name(model_path.stem + '_loss').with_suffix('.png'))
     plt.show()
 
 
-if __name__ == '__main__':
-    # command-line arguments
-    parser = argparse.ArgumentParser(description='Train classifier on COVIDx dataset.')
-    parser.add_argument('--data', type=str, help='Path to COVIDx dataset', required=True)
-    parser.add_argument('--model', type=str, help='Path where to save the trained model', required=True)
-    parser.add_argument('--logs', type=str, help='Path where to save logs for TensorBoard', default='logs')
-    args = parser.parse_args()
-
-    # prepare paths
-    dataset_path = Path(args.data)
-    model_path = Path(args.model)
-    logs_path = Path(args.logs)
-    model_path.parent.mkdir(parents=True, exist_ok=True)
-    logs_path.mkdir(parents=True, exist_ok=True)
-
+def train(dataset_path, model_path, logs_path):
     # build input pipeline
     train_ds = get_dataset(dataset_path / 'train')
     val_ds = get_dataset(dataset_path / 'validation')
@@ -72,67 +60,57 @@ if __name__ == '__main__':
     n_classes = train_ds.num_classes
 
     # plot one image (for debugging)
-    images = next(iter(train_ds))[0]
-    for image in images[:10]:
-        image = preprocess_input(image)
-        window_name = 'Example'
-        cv2.imshow(window_name, image)
-        cv2.waitKey(1000)
-        cv2.destroyWindow(window_name)
+    # images = next(iter(train_ds))[0]
+    # for image in images[:10]:
+    #     image = preprocess_input(image)
+    #     window_name = 'Example'
+    #     cv2.imshow(window_name, image)
+    #     cv2.waitKey(1000)
+    #     cv2.destroyWindow(window_name)
 
     # build model
-    base = ResNet50V2(include_top=False, pooling='avg', input_shape=INPUT_SHAPE)
-    head = Dense(n_classes)
+    feature_extractor = ResNet50V2(include_top=False, pooling='avg', input_shape=INPUT_SHAPE)
     inputs = Input(shape=INPUT_SHAPE)
     x = preprocess_input(inputs)
-    x = base(x, training=False)
-    outputs = head(x)
+    x = feature_extractor(x, training=False)  # training=False to keep BN layers in inference mode when unfrozen
+    outputs = Dense(n_classes)(x)
     model = Model(inputs, outputs)
 
     # training settings
-    learning_rate = 0.001
+    lr = 0.001
+    lr_ft = lr / 10
     epochs = 20
-    loss = CategoricalCrossentropy(from_logits=True)
+    epochs_ft = epochs + 10
     metrics = ['accuracy']
+    loss = CategoricalCrossentropy(from_logits=True)
     callbacks = [
         ModelCheckpoint(filepath=model_path, monitor='val_accuracy', mode='max', save_best_only=True, verbose=2),
         # TensorBoard(log_dir=logs_path)
     ]
 
-    # train only head (feature extraction with pre-trained model)
-    base.trainable = False
-    model.compile(optimizer=Adam(lr=learning_rate), loss=loss, metrics=metrics)
+    # train only top layer (feature extraction with pre-trained convolutional base)
+    feature_extractor.trainable = False
+    model.compile(optimizer=Adam(lr=lr), loss=loss, metrics=metrics)
     model.summary()
     history = model.fit(train_ds, epochs=epochs, validation_data=val_ds, callbacks=callbacks)
-    plt.figure()
-    plot_learning_curves(history)
+    plot_learning_curves(model_path, history)
 
     # fine-tune some layers
-    learning_rate /= 10
-    epochs += 10
     fine_tune_at = 20
-    base.trainable = True
-    for layer in base.layers[:fine_tune_at]:
+    feature_extractor.trainable = True
+    for layer in feature_extractor.layers[:fine_tune_at]:
         layer.trainable = False
-    model.compile(optimizer=Adam(lr=learning_rate), loss=loss, metrics=metrics)
+    model.compile(optimizer=Adam(lr=lr_ft), loss=loss, metrics=metrics)
     model.summary()
-    history_ft = model.fit(train_ds, epochs=epochs, initial_epoch=history.epoch[-1]+1, validation_data=val_ds,
+    history_ft = model.fit(train_ds, epochs=epochs_ft, initial_epoch=epochs, validation_data=val_ds,
                            callbacks=callbacks)
-    plot_learning_curves(history, history_ft)
+    plot_learning_curves(model_path, history, history_ft)
     model.save(model_path)
 
-    # evaluate model
-    # TODO: night's test should give over-fitting (>90% training accuracy, poor generalization)
-    # TODO: add confusion matrix and other metrics for evaluation (code below) -> should suck now
-    # TODO: add data augmentation -> over-fitting should be reduced (i.e. test accuracy should improve)
-    # TODO: add resampling -> confusion matrix should improve
-    # TODO: add metric for cross-validation, val_acc is not adequate due to the imbalance (e.g. f1 score, but we have
-    #  3 classes..., google it)
-    # TODO: add grad-cam
-    # TODO: add COVID-Net
-    # TODO: run tests
-    # TODO: image saving and model loading
 
+def evaluate(model_path):
+    load_model(model_path)
+    print('here you are')
     # confusion matrix
     # cm = confusion_matrix(test_data_gen.classes, predictions)
     # ticks = np.arange(n_classes)
@@ -149,3 +127,37 @@ if __name__ == '__main__':
     # print(cr)
     # with open(os.path.join(args.results, model_name + '_report.txt'), mode='w') as f:
     #     f.write(cr)
+
+
+if __name__ == '__main__':
+    # command-line arguments
+    parser = argparse.ArgumentParser(description='Train classifier on COVIDx dataset.')
+    parser.add_argument('data', type=str, help='Path to COVIDx dataset')
+    parser.add_argument('models', type=str, help='Path where to save the trained model')
+    parser.add_argument('model', type=str, help='Model name. If a file <model>.h5 exists, the model will be loaded and '
+                                                'just evaluated. Otherwise, it will be trained, saved and evaluated.')
+    parser.add_argument('--logs', type=str, default='logs', help='Path where to save logs for TensorBoard')
+    args = parser.parse_args()
+
+    # prepare paths
+    dataset_path = Path(args.data)
+    model_path = Path(args.models) / args.model
+    model_path = model_path.with_suffix('.h5')
+    logs_path = Path(args.logs)
+    model_path.parent.mkdir(parents=True, exist_ok=True)
+    logs_path.mkdir(parents=True, exist_ok=True)
+
+    # train and evaluate model
+    if not model_path.is_file():
+        train(dataset_path, model_path, logs_path)
+    evaluate(model_path)
+
+
+# TODO: add confusion matrix and other metrics for evaluation (code commented in evaluate()) -> should suck now
+# TODO: add data augmentation -> over-fitting should be reduced (i.e. test accuracy should improve)
+# TODO: add resampling -> confusion matrix should improve
+# TODO: add metric for cross-validation, val_acc is not adequate due to the imbalance (e.g. f1 score, but we have
+#  3 classes..., google it)
+# TODO: add grad-cam
+# TODO: add COVID-Net
+# TODO: run tests
